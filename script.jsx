5e3f1299f27bd842bab0c508377a693efe9e2a47
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-01-29 19:42:30 +0530

    Add files via upload

---
+++ script.jsx
@@ -0,0 +1,9 @@
+// script.jsx
+function Hello() {
+  return <h1>Hello World!</h1>;
+}
+
+const container = document.getElementById('mydiv');
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />);
+
