0f59d3d845134bea576a7856a3b14e2f3915e425
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-01-29 20:01:36 +0530

    Update script.jsx

--- script.jsx
+++ script.jsx
@@ -1,9 +1,22 @@
-// script.jsx
-function Hello() {
-  return <h1>Hello World!</h1>;
+import React from 'react';
+
+class MyComponent extends React.Component {
+  // Your custom method
+  myFunction = () => {
+    console.log("Hello from myFunction!");
+  };
+
+  render() {
+    // Call your custom method in the JSX file
+    this.myFunction();
+
+    return (
+      <div>
+        {/* Your JSX content */}
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
 }
 
-const container = document.getElementById('mydiv');
-const root = ReactDOM.createRoot(container);
-root.render(<Hello />);
-
+export default MyComponent;
