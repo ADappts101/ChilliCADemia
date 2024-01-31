e19547a80df56e6c6e5511f8e5e69eff4eb0e3b7
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-01-31 20:17:32 +0530

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,6 +1,6 @@
-// script.mjs
-import React from 'react';
-import ReactDOM from 'react-dom';
+// script.jsx
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 function Hello() {
   return React.createElement('h1', null, 'Hello World!');
